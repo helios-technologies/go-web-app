5227e78571230075fec20e033c2f9091a3e8587a
Camille Meulien <camille.meulien@gmail.com>
2017-03-01 10:48:21 +0100

    Add the ability to set the version on build with TAG variable

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
-TAG ?= latest
+TAG ?= onbuild
+LDFLAGS += -X main.Version=$(TAG)
+
+build:
+	go build -ldflags "$(LDFLAGS)" .
 
 image:
 	docker build -t heliostech/go-web-app:$(TAG) .
--- main.go
+++ main.go
@@ -7,12 +7,16 @@ import (
 	"fmt"
 )
 
+var Version = "onbuild"
+
 func indexHandler(res http.ResponseWriter, req *http.Request) {
-	io.WriteString(res, "Hello world!")
+	io.WriteString(res, "Hello world!\n")
+	io.WriteString(res, fmt.Sprintf("Web application version: %s\n", Version))
 }
 
 func main() {
 	http.HandleFunc("/", indexHandler)
 	fmt.Println("Listening on 8080")
+	fmt.Printf("Web application version: %s\n", Version)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
